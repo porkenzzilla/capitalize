4b6169311575ba14f92bfb6d1d7ef829d841ef88
Oleh Yuzva <olegyuzva22@gmail.com>
2021-05-16 13:32:06 +0300

    add src and tests

---
+++ tests/capitalize.test.js
@@ -0,0 +1,9 @@
+if (capitalize('hello') !== 'Hello') {
+	  throw new Error('Функция работает неверно!');
+}
+
+if (capitalize('') !== '') {
+	  throw new Error('Функция работает неверно!');
+}
+
+console.log('Все тесты пройдены!');
